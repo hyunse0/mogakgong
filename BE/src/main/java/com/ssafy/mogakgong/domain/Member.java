c76fb78198814c9a818e882a4d938be1cd9d39e8
SSAFY <gkgustj@naver.com>
2022-02-07 07:47:22 +0900

    Feat: 1:1 문의 기능

--- BE/src/main/java/com/ssafy/mogakgong/domain/Member.java
+++ BE/src/main/java/com/ssafy/mogakgong/domain/Member.java
@@ -1,6 +1,8 @@
 package com.ssafy.mogakgong.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -15,40 +17,51 @@ import java.util.List;
 @Entity
 @Table(name = "member")
 @Getter @Setter
+@ApiModel(value = "Member : 회원정보", description = "회원의 상세 정보를 나타낸다.")
 public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
+    @ApiModelProperty(value = "회원 번호")
     private Integer id;
 
     @Column(name = "email")
+    @ApiModelProperty(value = "회원 이메일")
     private String email;
 
     @Column(name = "password")
+    @ApiModelProperty(value = "회원 비밀번호")
     private String password;
 
     @Column(name = "nickname")
+    @ApiModelProperty(value = "회원 닉네임")
     private String nickname;
 
     @Column(name = "img")
+    @ApiModelProperty(value = "회원 프로필 이미지")
     private String img;
 
     @Column(name = "birth")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @ApiModelProperty(value = "회원 생년월일")
     private Date birth;
 
     @Column(name = "goal")
+    @ApiModelProperty(value = "회원 목표")
     private String goal;
 
     @Column (name = "reg_date")
+    @ApiModelProperty(value = "회원 가입일")
     @CreationTimestamp
     private Timestamp regDate;
 
     @Column(name = "is_exist")
+    @ApiModelProperty(value = "회원 탈퇴여부")
     private Integer isExist;
 
     @Column(name = "type")
+    @ApiModelProperty(value = "회원 등급?")
     private String type;
 
 }
