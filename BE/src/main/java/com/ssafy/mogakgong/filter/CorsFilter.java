7834df9e4c0b447bc4e02e677b4e85ab425c9235
김일환 <ikhwan62@naver.com>
2022-02-15 02:06:22 +0900

    Feat: 토큰 값 주면 회원 ID 반환하는 api 수정

--- BE/src/main/java/com/ssafy/mogakgong/filter/CorsFilter.java
+++ BE/src/main/java/com/ssafy/mogakgong/filter/CorsFilter.java
@@ -32,7 +32,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods","*");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
-                "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+                "Origin, X-Requested-With, Content-Type, Accept");
         response.addHeader("Access-Control-Expose-Headers", "Authorization");
 
         if("OPTIONS".equalsIgnoreCase(request.getMethod())) {
